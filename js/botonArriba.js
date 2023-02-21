c662afa8c7e31815345421429a2bf359cfd15fa6
thiagofrivs <thiagofrivs01@gmail.com>
2023-02-21 18:08:54 -0300

    Rellenado el about y los proyectos || falta cambiar la foto.

--- js/botonArriba.js
+++ js/botonArriba.js
@@ -1,13 +1,13 @@
 const toTop = (() => {
     let button = document.getElementById("toTop");
     window.onscroll = () => {
-    button.classList[
+        button.classList[
         (document.documentElement.scrollTop > 200) ? "add" : "remove"
         ]("is-visible")
     }
     button.onclick = () => {
-    window.scrollTo({
+        window.scrollTo({
         top:0, behavior:"smooth"
-    })
+        })
     }
 })();
